218c7d003e7dcfa3a91823314923ce48ec614113
shitakemura <shitakemura@gmail.com>
2022-05-08 03:37:58 +0900

    setup and implement apollo client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { ApolloProvider } from '@apollo/client'
 import type { NextPage } from 'next'
+import { client } from '../apollo-client'
 
 const HomePage: NextPage = () => {
-  return <h2>Next Apollo Sample App</h2>
+  return (
+    <ApolloProvider client={client}>
+      <h2>Next Apollo Sample App</h2>
+    </ApolloProvider>
+  )
 }
 
 export default HomePage
