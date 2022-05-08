d1c28cd2a678ee1b19cbba64504aeb80a0e02db4
shitakemura <shitakemura@gmail.com>
2022-05-08 10:09:29 +0900

    implement getTodos query calling

--- components/TodoList/TodoList.tsx
+++ components/TodoList/TodoList.tsx
@@ -1,16 +1,13 @@
-import { Todo } from '../../graphql/dist/generated-client'
+import { useQuery } from '@apollo/client'
+import { GetTodosDocument } from '../../graphql/dist/generated-client'
 import { TodoItem } from '../TodoItem.tsx'
 
 export const TodoList = () => {
-  const todos: Todo[] = [
-    { id: '1', title: 'Todo1', completed: false },
-    { id: '2', title: 'Todo2', completed: true },
-    { id: '3', title: 'Todo3', completed: false },
-  ]
+  const { data } = useQuery(GetTodosDocument)
 
   return (
     <div>
-      {todos.map((todo) => (
+      {data?.todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
