90fa37d2612f5c85f3b959962c8d398b19f748bf
shitakemura <shitakemura@gmail.com>
2022-05-09 23:02:07 +0900

    move apollo provider setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
-import { ApolloProvider } from '@apollo/client'
 import type { NextPage } from 'next'
-import { client } from '../apollo-client'
 import { CreateTodo } from '../components/todos/CreateTodo'
 import { TodoList } from '../components/todos/TodoList'
 
 const HomePage: NextPage = () => {
   return (
-    <ApolloProvider client={client}>
+    <div>
       <h2>Next Apollo Sample App</h2>
       <CreateTodo />
       <TodoList />
-    </ApolloProvider>
+    </div>
   )
 }
 
