5877ebff8de64a0bb3fe1c444c39ead3ea977ba5
shitakemura <shitakemura@gmail.com>
2022-05-09 23:17:08 +0900

    update layout

--- components/auth/Login/Login.tsx
+++ components/auth/Login/Login.tsx
@@ -1,3 +1,33 @@
+import styles from './Login.module.css'
+import { useState } from 'react'
+
 export const Login = () => {
-  return <div>Login</div>
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  })
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <input
+          type='text'
+          placeholder='Your email address'
+          value={formState.email}
+          onChange={(e) =>
+            setFormState({ ...formState, email: e.target.value })
+          }
+        />
+        <input
+          type='text'
+          placeholder='Your password'
+          value={formState.password}
+          onChange={(e) =>
+            setFormState({ ...formState, password: e.target.value })
+          }
+        />
+      </div>
+      <button>Login</button>
+    </div>
+  )
 }
