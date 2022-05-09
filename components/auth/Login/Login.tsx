a8eb45049191d1f59a079ecf89d4d88654b930ba
shitakemura <shitakemura@gmail.com>
2022-05-10 00:02:28 +0900

    implement login mutation calling

--- components/auth/Login/Login.tsx
+++ components/auth/Login/Login.tsx
@@ -1,10 +1,24 @@
 import styles from './Login.module.css'
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { LoginDocument } from '../../../graphql/dist/generated-client'
+import { useRouter } from 'next/router'
 
 export const Login = () => {
   const [formState, setFormState] = useState({
     email: '',
     password: '',
+  })
+
+  const router = useRouter()
+  const [login] = useMutation(LoginDocument, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ login }) => {
+      router.push('/')
+    },
   })
 
   return (
@@ -27,7 +41,7 @@ export const Login = () => {
           }
         />
       </div>
-      <button>Login</button>
+      <button onClick={() => login()}>Login</button>
     </div>
   )
 }
