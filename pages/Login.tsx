baef00b501d9b0b0700f6b85cca89d8cc4cb58ce
shitakemura <shitakemura@gmail.com>
2022-05-09 22:47:08 +0900

    create Login page

---
+++ pages/Login.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import { Login } from '../components/auth/Login'
+
+const LoginPage: NextPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
