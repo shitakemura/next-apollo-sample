c28390e163abc320e5abe265c0ce9e5925a6bea3
shitakemura <shitakemura@gmail.com>
2022-05-13 16:29:15 +0900

    check login status

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -2,8 +2,11 @@ import { NextPage } from 'next'
 import { Layout } from '../components/layout'
 import { CreateTodo } from '../components/todos/CreateTodo'
 import { TodoList } from '../components/todos/TodoList'
+import { useRequiredLogin } from '../hooks/useRequireLogin'
 
 const TodosPage: NextPage = () => {
+  useRequiredLogin()
+
   return (
     <Layout>
       <CreateTodo />
