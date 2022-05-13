1beae1c2f34bb8a71c2517c5a0777c2c71d7ac48
shitakemura <shitakemura@gmail.com>
2022-05-14 00:25:30 +0900

    set token to auth header of todos query

--- src/components/todos/TodoList/TodoList.tsx
+++ src/components/todos/TodoList/TodoList.tsx
@@ -1,9 +1,17 @@
 import { useQuery } from '@apollo/client'
 import { GetTodosDocument } from '../../../graphql/dist/generated-client'
+import { useAuthState } from '../../../hooks/useAuth'
 import { TodoItem } from '../TodoItem.tsx'
 
 export const TodoList = () => {
-  const { data } = useQuery(GetTodosDocument)
+  const { authPayload } = useAuthState()
+  const token = authPayload?.token ?? ''
+
+  const { data } = useQuery(GetTodosDocument, {
+    context: {
+      headers: { authorization: `Bearer ${token}` },
+    },
+  })
 
   return (
     <div>
